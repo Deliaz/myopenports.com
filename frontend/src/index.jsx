eae1fc0ffa9b4cc8c2be68a4e7e0452a9748751a
Deliaz <D3liaz@gmail.com>
2019-03-25 18:46:13 +0700

    [FE] Add primitive Sentry integration

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,7 +6,12 @@ import App from './App';
 import {I18nextProvider} from 'react-i18next';
 import i18n from './i18n';
 import { YMInitializer } from 'react-yandex-metrika';
+import * as Sentry from '@sentry/browser';
 
+if (process.env.NODE_ENV === 'production') {
+	// Init Sentry integration
+	Sentry.init({dsn: 'https://e3e1cef0a9db45a88c11c3cfb53ce7bf@sentry.io/1423042'});
+}
 
 ReactDOM.render(
 	<I18nextProvider i18n={i18n}>
